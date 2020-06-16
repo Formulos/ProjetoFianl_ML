df183619ef112338dc25bb39557ca9fbb5d94f84
formulos <paulotozp@gmail.com>
2020-06-16 18:51:15 -0300

    raytracing

--- Assets/scripts/RollerAgent.cs
+++ Assets/scripts/RollerAgent.cs
@@ -11,6 +11,7 @@ public class RollerAgent : Agent
     Rigidbody rBody;
     int hunger;
     private int foodQuantity;
+    public GameObject spawnZone;
 
     //float starving = 0;
     void Start () 
@@ -30,7 +31,7 @@ public class RollerAgent : Agent
     {        
         
 
-        hunger = 5;
+        hunger = 10;
         rBody = this.GetComponent<Rigidbody>();
         //starving = 0;
         //world.updateState(0);
@@ -42,11 +43,11 @@ public class RollerAgent : Agent
         this.rBody.angularVelocity = Vector3.zero;
         this.rBody.velocity = Vector3.zero;
 
-        float xmin = this.transform.parent.position.x - this.transform.parent.transform.lossyScale.x*3;
-        float xmax = this.transform.parent.position.x + this.transform.parent.lossyScale.x*3;
+        float xmin = spawnZone.transform.position.x - spawnZone.transform.lossyScale.x*3;
+        float xmax = spawnZone.transform.position.x + spawnZone.transform.lossyScale.x*3;
 
-        float zmin = this.transform.parent.position.z - this.transform.parent.lossyScale.z*3;
-        float zmax = this.transform.parent.position.z + this.transform.parent.lossyScale.z*3;
+        float zmin = spawnZone.transform.position.z - spawnZone.transform.lossyScale.z*3;
+        float zmax = spawnZone.transform.position.z + spawnZone.transform.lossyScale.z*3;
 
         Vector3 pos = new Vector3(Random.Range(xmin, xmax), 0.5f, Random.Range(zmin, zmax));
 
@@ -72,44 +73,71 @@ public class RollerAgent : Agent
         //print("z_velocity: " + (rBody.velocity.z).ToString());
 
         //sensor.AddObservation(this.starving);
-
         //Vector2 rpos = new Vector2(this.transform.localPosition.x, this.transform.localPosition.z);
-        for(int i=0;i<foodQuantity;i++){
+/*         for(int i=0;i<foodQuantity;i++){
             //print(worldState[10].transform.localPosition);
             Vector3 foodPosition = worldState[i].transform.localPosition;
-            sensor.AddObservation(foodPosition);
-            //print("Food_position: " + (foodPosition).ToString());
-            //sensor.AddObservation(Vector2.Distance(rpos,foodPosition));
-            //print("dists: "+ (Vector2.Distance(rpos,foodPosition)).ToString());
-/*             for(int j=0;j<2;j++){
-                //sensor.AddObservation(worldState[i,j]);
-                if(worldState[i,2]==0){
-                    print("i: "+i.ToString());
-                    print("xpos: "+worldState[i,0].ToString());
-                    print("zpos: "+worldState[i,1].ToString());
-                }
-                
-            } */
-        } 
+            sensor.AddObservation(foodPosition.x);
+            sensor.AddObservation(foodPosition.z);
+            sensor.AddObservation(Vector3.Distance(foodPosition, this.transform.localPosition));
+
+        }  */
     }
 
-    public float speed = 10;
+    public float speed = 2f;
     public override void OnActionReceived(float[] action)
         {
             
-            Vector3 controlSignal = Vector3.zero;
-            controlSignal.x = action[0];
-            controlSignal.z = action[1];
-            rBody.AddForce(controlSignal * speed);
-            if(this.transform.localPosition.y < -0.1f){
-                AddReward(-1f);
-                EndEpisode();
+            //Vector3 controlSignal = Vector3.zero;
+            //controlSignal.x = action[0];
+            //controlSignal.z = action[1];
+            //rBody.AddForce(controlSignal * speed);
+
+            Vector3 dirToGo = Vector3.zero;
+            //var rotateDir = Vector3.zero;
+
+            //print("action 0 :" + action[0]);
+            //print("action 1 :" + action[1]);
+
+            int forwardAxis = 0;
+            int rightAxis = 0;
+
+            float threshold = 0.25f;
+
+            if(action[0] > threshold){
+                forwardAxis = 1;
+            } else if(action[0] < -threshold){
+                forwardAxis = -1;
             }
+
+            if(action[1] > threshold){
+                rightAxis = 1;
+            } else if(action[1] < -threshold){
+                rightAxis = -1;
+            }   
+
+            dirToGo.x = forwardAxis;
+            dirToGo.z = rightAxis;
+
+            rBody.AddForce(dirToGo * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+            //transform.Rotate(rotateDir, Time.fixedDeltaTime * speed);
+
+
+
+/*         if (rBody.velocity.sqrMagnitude > 150f) // slow it down
+        {
+            rBody.velocity *= 0.95f;
+        } */
+        if(this.transform.localPosition.y < -0.1f){
+            AddReward(-1f);
+            EndEpisode();
+        }
         //penalidade de morrer de fome
         AddReward(-0.0005f);
         //print(starving);
         //print("penalt"+ (-0.0005f*starving).ToString());
         //starving +=0.5f;
+
 
         }
     
@@ -120,13 +148,13 @@ public class RollerAgent : Agent
             int id = collision.gameObject.GetComponent<Food>().id;
             //print("debug pre pos: "+(other.transform.position).ToString());
             //print("debug pre pos: "+(worldState[id].transform.localPosition).ToString());
-            world.updateState(id);
             Destroy(collision.gameObject);
+            world.updateState(id);
             AddReward(1f);
             //starving = 0;
             hunger--;
             if(hunger == 0){
-                //SetReward(2f);
+                SetReward(2f);
                 //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
                 EndEpisode();
             }
@@ -137,7 +165,23 @@ public class RollerAgent : Agent
     
     public override void Heuristic(float[] actionsOut)
     {
-        actionsOut[0] = Input.GetAxis("Horizontal");
-        actionsOut[1] = Input.GetAxis("Vertical");
+        actionsOut[0] = 0f;
+        actionsOut[1] = 0f;
+        if (Input.GetKey(KeyCode.A))
+        {
+            actionsOut[0] = -1f;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            actionsOut[1] = 1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            actionsOut[0] = 1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            actionsOut[1] = -1f;
+        }
     }
 }
